e3d71d120bd726baa52cfb72c0a02e996a9a9cca
morcath <aleksander.tym@gmail.com>
2019-01-14 12:13:35 +0100

    add architecture of ga with history

--- experiments.R
+++ experiments.R
@@ -1,15 +1,16 @@
+# experiments and main loop of GA by Aleksander Tym and Filip Kuchta
 #load packages
 
-library(GA)
 library(cec2013)
+source('GA_with_history.R')
 
 function_indexes <- seq(from = 1, to = 28, length = 28)
 function_optima <- c(seq(from = -1400, to = -100, length = 14),
                      seq(from = 100, to = 1400, length = 14))
 function_dim = 10
 
-runs_num = 50
-functions_num = 28
+runs_num = 1
+functions_num = 1#28
 dimensions_num = 1
 set.seed(1)
 
@@ -31,13 +32,15 @@ for(i in 1:functions_num)
     }
     
     result <- ga(type="real-valued",
-                 maxiter = 100,
+                 maxiter = 3,
+                 useHistory = TRUE,
                  fitness=f,
                  popSize=populationSize,
                  pcrossover = probabilityCrossover, 
                  pmutation = probabilityMutation,
                  lower=rep(-100, function_dim),
                  upper=rep(100, function_dim))
+    print(result)
     solution <- summary(result)$solution
     error <- cec2013(i, solution) - function_optima[i]
     results <- c(results, error[1])
